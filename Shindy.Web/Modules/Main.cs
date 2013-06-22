bf79c591935e8a1ceaddfc7a59030c3f6120f9d3
Codewhisperer <dnico81@gmail.com>
2013-06-22 12:28:22 -0400

    Created Nancy solution

---
+++ Shindy.Web/Modules/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace Shindy.Web.Modules
+{
+    public class Main :NancyModule
+    {
+        public Main()
+        {
+            Get["/"] = x =>
+            {
+                return View["Index"];
+            };
+        }
+
+    }
+}
