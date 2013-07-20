a070f77067c0bda9543134df2dba2a58e80ecbae
ptimothee <patrick.timothee@gmail.com>
2013-07-20 16:49:36 -0400

    Implemented create person

--- Shindy.Data/Person.cs
+++ Shindy.Data/Person.cs
@@ -1,26 +1,112 @@
-﻿using System;
+﻿using Simple.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Simple.Data.Ado;
+using System.Dynamic;
+using System.Configuration;
 
 namespace Shindy.Data
 {
     public class Person
     {
-        private Person()
-        {
+        internal readonly static string ConnectionString = ConfigurationManager.ConnectionStrings["Shindy.Data"].ToString();
 
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
         }
 
-        public Person Create()
+        public int PersonID { get; private set; }
+
+        private string _firstName;
+        public string FirstName {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                _firstName = value;
+            }
+        }
+
+        private string _lastName;
+        public string LastName {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                _lastName = value;
+            }
+        }
+
+        public string Email { get; set; }
+
+        public string MemberURI { get; set; }
+
+        public string TwitterName {get; set;}
+
+        public string LinkedinURI { get; set; }
+
+        public string Bio { get; set; }
+
+        public string PhotoURI {get; set;}
+
+        public string Zip { get; set; }
+
+        public void Persist()
+        {
+            if (PersonID <= 0)
+            {
+                Create();
+            }
+            else
+            {
+                Update();
+            }            
+        }
+
+        public static Person Load(int personId)
         {
             throw new NotImplementedException();
         }
 
-        public Person Load(int personId)
+        public void Update()
         {
             throw new NotImplementedException();
+        }
+
+        public void Create()
+        {
+            var db = Database.Opener.OpenConnection(ConnectionString);
+
+            var person = db.Persons.Insert(
+                First_Name: FirstName,
+                Last_Name: LastName,
+                EMail: Email,
+                Member_URI: MemberURI,
+                Twitter_Name: TwitterName,
+                Linkedin_URI: LinkedinURI,
+                Bio: Bio,
+                Photo_URI: PhotoURI,
+                Zip: Zip
+                );
+
+            PersonID = person.Person_ID;
         }
     }
 }
---
+++ src/Shindy.Tests/Data/PersonTests.cs
@@ -0,0 +1,42 @@
+﻿using Shindy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Shindy.Tests.Data
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void CreatePerson()
+        {
+            //Arrange
+            Person person = new Person("Donald", "Duck");
+            person.Email = "donald.duck@disney.com";
+
+            //Act
+            person.Persist();
+
+            //Assert
+            Assert.True(person.PersonID > 0);
+        }
+    }
+
+    //Please excuse my ignorance. I have never used Xunit before
+    //and do not know how to run this "fact". I switched the project type
+    //to console and use the below to test my code (Patrick)
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PersonTests test = new PersonTests();
+            test.CreatePerson();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
