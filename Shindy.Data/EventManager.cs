e930ccbda71f59f198cb0117090d9bcd931c150f
ptimothee <patrick.timothee@gmail.com>
2013-07-20 11:48:03 -0400

    Added some classes to the Shindy.Data proj

---
+++ Shindy.Data/EventManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shindy.Data
+{
+    public static class EventManager
+    {
+        public static void AddSpeaker(this Event evt, Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    
+    }
+}
---
+++ Shindy.Data/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shindy.Data
+{
+    public class Person
+    {
+        private Person()
+        {
+
+        }
+
+        public Person Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Load(int personId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
