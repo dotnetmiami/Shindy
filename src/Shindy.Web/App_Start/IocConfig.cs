1f94fe77bab28cea1ca7091df15151f0c16abf65
Janier Davila <janier_davila@yahoo.com>
2015-03-25 16:14:00 -0400

    Added Autofac and wired some dependecies

---
+++ src/Shindy.Web/App_Start/IocConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Shindy.Data.SqlServer;
+
+namespace Shindy.Web
+{
+    public static class IocConfig
+    {
+        public static void Configure()
+        {
+            // Create the container builder.
+            var builder = new ContainerBuilder();
+
+            // Register the Web API controllers.
+            var asm = Assembly.GetExecutingAssembly();
+            builder.RegisterApiControllers(asm);
+            
+
+            // Register other dependencies.
+            builder.RegisterType<ShindyContext>().As<IShindyContext>().InstancePerRequest();
+            
+            // Build the container.
+            var container = builder.Build();
+
+            // Create the depenedency resolver.
+            var resolver = new AutofacWebApiDependencyResolver(container);
+
+            // Configure Web API with the dependency resolver.
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        } 
+    }
+}
---
+++ src/Shindy.Web/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace Shindy.Web
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            IocConfig.Configure();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
